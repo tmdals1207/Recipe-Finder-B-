a2869e32076885631e50d007c2d929c55d5775dc
tmdals1207 <ghdtmdalsdad@naver.com>
2024-08-07 17:39:00 +0900

    init

---
+++ src/main/java/com/hong/recipe_finder/RecipeFinderApplication.java
@@ -0,0 +1,13 @@
+package com.hong.recipe_finder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeFinderApplication.class, args);
+	}
+
+}

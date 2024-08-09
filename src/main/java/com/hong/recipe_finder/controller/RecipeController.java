c575373b355a5db34c2d7bb0b5966778a359df68
tmdals1207 <ghdtmdalsdad@naver.com>
2024-08-09 10:16:13 +0900

    Controller가 Service를 호출하도록 변경

--- src/main/java/com/hong/recipe_finder/controller/RecipeController.java
+++ src/main/java/com/hong/recipe_finder/controller/RecipeController.java
@@ -2,55 +2,53 @@ package com.hong.recipe_finder.controller;
 
 import com.hong.recipe_finder.model.Recipe;
 import com.hong.recipe_finder.repository.RecipeRepository;
+import com.hong.recipe_finder.service.RecipeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/recipes")
+@CrossOrigin(origins = "http://localhost:4173") // 프론트엔드 포트
 public class RecipeController {
 
     @Autowired
-    private RecipeRepository recipeRepository;
+    private RecipeService recipeService; // Service 계층 사용
 
     @GetMapping
-    public List<Recipe> getAllRecipes() {
-        return recipeRepository.findAll();
+    public ResponseEntity<List<Recipe>> getAllRecipes() {
+        List<Recipe> recipes = recipeService.getAllRecipes();
+        return ResponseEntity.ok(recipes);
     }
 
     @PostMapping
-    public Recipe createRecipe(@RequestBody Recipe recipe) {
-        return recipeRepository.save(recipe);
+    public ResponseEntity<Recipe> createRecipe(@RequestBody Recipe recipe) {
+        Recipe createdRecipe = recipeService.createRecipe(recipe);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdRecipe);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Recipe> getRecipeById(@PathVariable Long id) {
-        return recipeRepository.findById(id)
+        return recipeService.getRecipeById(id)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<Recipe> updateRecipe(@PathVariable Long id, @RequestBody Recipe recipeDetails) {
-        return recipeRepository.findById(id)
-                .map(recipe -> {
-                    recipe.setName(recipeDetails.getName());
-                    recipe.setIngredients(recipeDetails.getIngredients());
-                    recipe.setInstructions(recipeDetails.getInstructions());
-                    return ResponseEntity.ok(recipeRepository.save(recipe));
-                })
+        return recipeService.updateRecipe(id, recipeDetails)
+                .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Object> deleteRecipe(@PathVariable Long id) {
-        return recipeRepository.findById(id)
-                .map(recipe -> {
-                    recipeRepository.delete(recipe);
-                    return ResponseEntity.noContent().build();
-                })
+        return recipeService.deleteRecipe(id)
+                .map(recipe -> ResponseEntity.noContent().build())
                 .orElse(ResponseEntity.notFound().build());
     }
 }

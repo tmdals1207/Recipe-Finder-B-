6c78c2b3b588cd6f15ce93f41fa8f4aa8b7cedd4
tmdals1207 <ghdtmdalsdad@naver.com>
2024-08-09 10:16:46 +0900

    RecipeController 테스트

---
+++ src/test/java/com/hong/recipe_finder/recipe/RecipeControllerTest.java
@@ -0,0 +1,27 @@
+package com.hong.recipe_finder.recipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class RecipeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testGetAllRecipes() throws Exception {
+        mockMvc.perform(get("/api/recipes"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$").isArray());
+    }
+}

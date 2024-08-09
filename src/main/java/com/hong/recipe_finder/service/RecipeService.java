76fc467bfd6e97846664972cc3e0364502e0d5b5
tmdals1207 <ghdtmdalsdad@naver.com>
2024-08-09 10:16:33 +0900

    RecipeService 생성 및 Repository 계층 사용으로 변경

---
+++ src/main/java/com/hong/recipe_finder/service/RecipeService.java
@@ -0,0 +1,46 @@
+package com.hong.recipe_finder.service;
+
+import com.hong.recipe_finder.model.Recipe;
+import com.hong.recipe_finder.repository.RecipeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class RecipeService {
+
+    @Autowired
+    private RecipeRepository recipeRepository; // Repository 계층 사용
+
+    public List<Recipe> getAllRecipes() {
+        return recipeRepository.findAll();
+    }
+
+    public Recipe createRecipe(Recipe recipe) {
+        return recipeRepository.save(recipe);
+    }
+
+    public Optional<Recipe> getRecipeById(Long id) {
+        return recipeRepository.findById(id);
+    }
+
+    public Optional<Recipe> updateRecipe(Long id, Recipe recipeDetails) {
+        return recipeRepository.findById(id)
+                .map(recipe -> {
+                    recipe.setName(recipeDetails.getName());
+                    recipe.setIngredients(recipeDetails.getIngredients());
+                    recipe.setInstructions(recipeDetails.getInstructions());
+                    return recipeRepository.save(recipe);
+                });
+    }
+
+    public Optional<Recipe> deleteRecipe(Long id) {
+        return recipeRepository.findById(id)
+                .map(recipe -> {
+                    recipeRepository.delete(recipe);
+                    return recipe;
+                });
+    }
+}

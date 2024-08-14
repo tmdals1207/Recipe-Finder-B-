3e72e6a07f1ef84cd9bc30effc81829fd3247e26
홍승민 <ghdtmdalsdad@naver.com>
2024-08-15 00:12:08 +0900

    [Feat] User 엔티티 추가

--- src/main/java/com/hong/recipe_finder/domain/User.java
+++ src/main/java/com/hong/recipe_finder/domain/User.java
@@ -1,23 +1,40 @@
 package com.hong.recipe_finder.domain;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
+import org.hibernate.annotations.DynamicUpdate;
 
 @Setter
 @Getter
 @Entity
+@DynamicUpdate
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
+@Builder
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private Long id;
 
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "email", nullable = false)
     private String email;
+
+    @Column(name = "password")
     private String password;
 
-    private String provider;  // e.g., google, facebook
-    private String providerId;
+    @Column(name = "provider")
+    private String provider;
 
+    // 사용자의 이름이나 이메일을 업데이트하는 메소드
+    public User updateUser(String username, String email) {
+        this.username = username;
+        this.email = email;
 
+        return this;
+    }
 }

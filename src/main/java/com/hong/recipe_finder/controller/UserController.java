3af7ac18d3de5c97d3724c4943ada8a51b4eeef6
홍승민 <ghdtmdalsdad@naver.com>
2024-08-15 00:12:44 +0900

    [Feat] 이메일, 비밀번호을 통한 회원가입, 로그인 추가

--- src/main/java/com/hong/recipe_finder/controller/UserController.java
+++ src/main/java/com/hong/recipe_finder/controller/UserController.java
@@ -1,30 +1,35 @@
 package com.hong.recipe_finder.controller;
 
 import com.hong.recipe_finder.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 import com.hong.recipe_finder.dto.UserDto;
 
+@Slf4j
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/api/auth")
+@CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
 
-    @Autowired
-    private UserService userService;
+    private final UserService userService;
 
-    @PostMapping("/signup")
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
     public ResponseEntity<String> signup(@RequestBody UserDto userDto) {
-        userService.signup(userDto);
+        log.info("회원가입 시작!");
+        userService.register(userDto);
         return ResponseEntity.ok("User registered successfully");
     }
 
     @PostMapping("/login")
     public ResponseEntity<String> login(@RequestBody UserDto userDto) {
+        log.info("로그인 시작!");
         boolean isAuthenticated = userService.login(userDto);
         if (isAuthenticated) {
             return ResponseEntity.ok("Login successful");

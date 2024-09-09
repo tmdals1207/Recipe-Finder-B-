ade4c389fd9aac0c7795b34b9341f3b578e0325a
홍승민 <ghdtmdalsdad@naver.com>
2024-09-09 12:03:44 +0900

    레시피 등록 로직 추가 및 관련 도메인 수정과 추가 (#11)
    * Controller가 Service를 호출하도록 변경
    
    * RecipeService 생성 및 Repository 계층 사용으로 변경
    
    * RecipeController 테스트
    
    * #2 코드 정리
    
    * CORS 해결 및 데이터 통신 확인
    
    * [Feat] 로그인 회원가입 기능 추가중
    
    * [Feat] User 관련 도메인, DTO 추가
    
    * [Feat] OAuth 로그인 로직 추가
    
    * [Feat] User가 OAuth가 아닌 아이디, 비밀번호 로그인 기능 추가
    
    * [Refactor] Recipe 관련 폴더명 등 리팩토링
    
    * [Feat] 패스워드 암호화 추가 및 환경 설정 변경
    
    * [Feat] OAuth2 registration, provider 추가
    
    * [Feat] 사용자 정보 반환 클래스 추가
    
    * [Feat] OAuth 2.0 로그인 추가(Google, Naver, Kakao)
    
    * [Feat] Security 설정 추가
    
    * [Feat] User 엔티티 추가
    
    * [Feat] 이메일, 비밀번호을 통한 회원가입, 로그인 추가
    
    * [Feat] OAuth 2.0 연결 설정 추가
    
    * [Feat] secret 포함 yml설정 gitignore에 추가
    
    * [Feat] secret 제거
    
    * [Fix] secret 재설정
    
    * [Fix] git ignore 재설정
    
    * [Fix] MemberController 삭제
    
    * [Fix] MemberRepository 삭제
    
    * [Feat] gitignore 추가
    
    * [Feat] jwt토큰 관련 키 값 추가
    
    * [Feat] Api 설정 추가
    
    * [Feat] jwt 토큰 관련 의존성 추가
    
    * [Feat] 토큰 속성 추가
    
    * [Feat] Spoonacular Api 사용하여 기능 추가
    
    * [Feat] Edamam Api 사용하여 기능 추가
    
    * [Feat] Edamam Api 사용하여 기능 추가
    
    * [Feat] 토큰 Provider, Filter추가
    
    * [Feat] 로그인 시 토큰 주고받는 Dto 추가
    
    * [Feat] 프론트와 통신 메소드 설정
    
    * [Feat] 로그인, 로그아웃 시 토큰 관리 하도록 추가
    
    * [Feat] Jwt 토큰 관련 Bean 추가
    
    * [Refacror] gitignore 추가
    
    * [Fix] OAuth 버그 수정 설정
    
    * [Fix] JWT 토큰 인증 정보 저장방식 변경
    
    * [Feat] 토큰을 통한 유저 인증기능 추가
    
    * [Feat] OAuth 로그인 후 리디렉션 방식 변경
    
    * [Fix] OAuth 로그인 시 provider 추가 인증하도록 변경
    
    * [Fix] OAuth 계정 로그아웃 안되는 오류 수정
    
    * [Feat] 레시피 등록을 위한 도메인 추가 및 변경
    
    * [Feat] 작동 확인을 위한 로그 추가
    
    * [Feat] userName의 중복 확인 로직 추가
    
    * [Feat] SMS 서비스를 위한 전화번호 속성 추가
    
    * [Fix] 변경된 Recipe 도메인에 맞게 메소드 변경중

--- src/main/java/com/hong/recipe_finder/domain/Recipe.java
+++ src/main/java/com/hong/recipe_finder/domain/Recipe.java
@@ -4,15 +4,124 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
-@Setter
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 @Getter
+@Setter
 @Entity
 public class Recipe {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String name;
-    private String ingredients;
-    private String instructions;
+    private Long id; // 고유 번호
 
+    private String profileImage; // 프로필 사진
+    private String summation; // 요약
+    private int views; // 조회수
+    private String authorProfile; // 작성자 프로필
+    private String title; // 제목
+    private String content; // 내용
+    private int servings; // 몇인분
+    private int cookingTime; // 걸리는 시간 (분 단위)
+    private String difficulty; // 난이도 (e.g. 쉬움, 중간, 어려움)
+
+    @ElementCollection
+    @CollectionTable(name = "recipe_ingredients", joinColumns = @JoinColumn(name = "recipe_id"))
+    private List<Ingredient> ingredients = new ArrayList<>(); // 재료
+
+    @ElementCollection
+    @CollectionTable(name = "recipe_seasonings", joinColumns = @JoinColumn(name = "recipe_id"))
+    private List<Ingredient> seasonings = new ArrayList<>(); // 양념
+
+    @Lob
+    private String knowHow; // 노하우
+
+    private String videoUrl; // 동영상 URL
+
+    @OneToMany(mappedBy = "recipe", cascade = CascadeType.ALL)
+    private List<CookingStep> cookingSteps = new ArrayList<>(); // 조리 순서
+
+    @Lob
+    private String tips; // 팁과 주의사항
+
+    private LocalDateTime createdDate; // 등록일
+    private LocalDateTime updatedDate; // 수정일
+
+    @OneToMany(mappedBy = "recipe", cascade = CascadeType.ALL)
+    private List<Review> reviews = new ArrayList<>(); // 리뷰
+
+    @ElementCollection
+    @CollectionTable(name = "recipe_tags", joinColumns = @JoinColumn(name = "recipe_id"))
+    private List<String> tags = new ArrayList<>(); // 태그
+
+    private double rating; // 별점
+
+    @OneToMany(mappedBy = "recipe", cascade = CascadeType.ALL)
+    private List<Comment> comments = new ArrayList<>(); // 댓글
+
+    // 영양정보 합산 필드
+    private double totalCalories;
+    private double totalFat;
+    private double totalSaturatedFat;
+    private double totalTransFat;
+    private double totalMonounsaturatedFat;
+    private double totalPolyunsaturatedFat;
+    private double totalCarbs;
+    private double totalFiber;
+    private double totalSugars;
+    private double totalProtein;
+    private double totalCholesterol;
+    private double totalSodium;
+    private double totalCalcium;
+    private double totalMagnesium;
+    private double totalPotassium;
+    private double totalIron;
+    private double totalPhosphorus;
+    private double totalVitaminA;
+    private double totalVitaminC;
+    private double totalThiaminB1;
+    private double totalRiboflavinB2;
+    private double totalNiacinB3;
+    private double totalVitaminB6;
+    private double totalFolateEquivalent;
+    private double totalVitaminB12;
+    private double totalVitaminD;
+    private double totalVitaminE;
+    private double totalVitaminK;
+
+
+    public void calculateTotalNutrition() {
+        for (Ingredient ingredient : ingredients) {
+            totalCalories += ingredient.getCalories();
+            totalFat += ingredient.getFat();
+            totalSaturatedFat += ingredient.getSaturatedFat();
+            totalTransFat += ingredient.getTransFat();
+            totalMonounsaturatedFat += ingredient.getMonounsaturatedFat();
+            totalPolyunsaturatedFat += ingredient.getPolyunsaturatedFat();
+            totalCarbs += ingredient.getCarbs();
+            totalFiber += ingredient.getFiber();
+            totalSugars += ingredient.getSugars();
+            totalProtein += ingredient.getProtein();
+            totalCholesterol += ingredient.getCholesterol();
+            totalSodium += ingredient.getSodium();
+            totalCalcium += ingredient.getCalcium();
+            totalMagnesium += ingredient.getMagnesium();
+            totalPotassium += ingredient.getPotassium();
+            totalIron += ingredient.getIron();
+            totalPhosphorus += ingredient.getPhosphorus();
+            totalVitaminA += ingredient.getVitaminA();
+            totalVitaminC += ingredient.getVitaminC();
+            totalThiaminB1 += ingredient.getThiaminB1();
+            totalRiboflavinB2 += ingredient.getRiboflavinB2();
+            totalNiacinB3 += ingredient.getNiacinB3();
+            totalVitaminB6 += ingredient.getVitaminB6();
+            totalFolateEquivalent += ingredient.getFolateEquivalent();
+            totalVitaminB12 += ingredient.getVitaminB12();
+            totalVitaminD += ingredient.getVitaminD();
+            totalVitaminE += ingredient.getVitaminE();
+            totalVitaminK += ingredient.getVitaminK();
+        }
+    }
 }
--- src/main/java/com/hong/recipe_finder/dto/UserDto.java
+++ src/main/java/com/hong/recipe_finder/dto/UserDto.java
@@ -8,13 +8,15 @@ import lombok.Setter;
 @Getter
 public class UserDto {
     private String username; // 사용자 이름
-    @Setter
+
     private String provider; // 로그인한 서비스
-    @Setter
+
     private String email; // 사용자의 이메일
-    @Setter
+
+    private String phone;
+
     private String password; //
-    @Setter
+
     private String token;
 
     public void setUserName(String userName) {
@@ -26,6 +28,7 @@ public class UserDto {
         return User.builder()
                 .username(this.username)
                 .email(this.email)
+                .phone(this.phone)
                 .provider(this.provider)
                 .password(this.password)
                 .token(this.token)

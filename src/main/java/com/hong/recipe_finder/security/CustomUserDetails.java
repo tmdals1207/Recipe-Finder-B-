c90c4d9a81860beb3d2cafc4cf1ff2f32b4fc3bc
홍승민 <ghdtmdalsdad@naver.com>
2024-12-31 23:36:58 +0900

    [Feat] JWT 인증 처리에서 기존의 UserDetails이 아닌 CustomUserDetails을 사용하도록 변경

---
+++ src/main/java/com/hong/recipe_finder/security/CustomUserDetails.java
@@ -0,0 +1,50 @@
+package com.hong.recipe_finder.security;
+
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+public class CustomUserDetails implements UserDetails, Serializable {
+    private final String email;
+    private final String username;
+
+    public CustomUserDetails(String email, String username) {
+        this.email = email;
+        this.username = username;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+}
